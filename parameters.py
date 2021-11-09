65d3840e7c5a8e9bf83073b8238d900632130636
Tim <frtim@student.ethz.ch>
2021-11-09 17:41:33 +0000

    uploaded segmentation volumes and soma mask

--- parameters.py
+++ parameters.py
@@ -15,10 +15,10 @@ batch_size = 4
 val_data_size = 384
 
 ## parameters for prediction ##
-ckpt_restore = '../SomaeDetection/ckpt/-40'
+ckpt_restore = 'ckpt/-40'
 
 # filepaths
-train_seg_in_filepath = "Mouse/seg_Mouse_762x832x832.h5"
-train_somae_in_filepath = "Mouse/somae_Mouse_762x832x832.h5"
-predict_seg_in_filepath = "Zebrafinch/Zebrafinch-seg_filled-dsp8.h5"
-somae_prediction_out_filepath = "Zebrafinch/Zebrafinch-somae_filled_new-dsp_8_2021.h5"
+train_seg_in_filepath = "Training_Data/JWR_segmentation.h5"
+train_somae_in_filepath = "Training_Data/JWR_somamask.h5"
+predict_seg_in_filepath = "Prediction_Data/Zebrafinch_segmentation.h5"
+somae_prediction_out_filepath = "Prediction_Data/Zebrafinch-somaepredicted.h5"
